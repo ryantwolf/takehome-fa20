4a5ac4465fc86daa66a5295a51d45274cef33745
repl.it user <replituser@example.com>
2020-09-09 22:14:04 +0000

    Finish react part 2

--- frontend/src/Counter.js
+++ frontend/src/Counter.js
@@ -2,10 +2,33 @@ import React, { Component } from 'react'
 
 class Counter extends Component {
   // YOUR CODE GOES BELOW
-  
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0
+    }
+  }
+
+  incrementCount = () => {
+    this.setState({
+      count: this.state.count+1
+    })
+  }
+
+  decrementCount = () => {
+    this.setState({
+      count: this.state.count-1
+    })
+  }
+
   render() {
     return (
-      <div/> 
+      <div>
+      The current count is {this.state.count}
+      <br />
+      <button onClick={this.incrementCount}>Increment</button>
+      <button onClick={this.decrementCount}>Decrement</button>
+      </div>
     )
   }
 }
