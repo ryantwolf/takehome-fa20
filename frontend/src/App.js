d21bd56fb769f18ce56339d2fc467a1eb51599e8
repl.it user <replituser@example.com>
2020-09-09 22:33:56 +0000

    Finish react part 5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,8 +11,25 @@ class App extends Component {
         {id: 1, name: "Golden Harbor", rating: 10},
         {id: 2, name: "Potbelly", rating: 6},
         {id: 3, name: "Noodles and Company", rating: 8},
-      ]
+      ],
+      newName: ""
     }
+  }
+
+  handleNameChage = event => {
+    this.setState({
+      newName: event.target.value
+    })
+  }
+
+  addRestaurant = () => {
+    const newId = Math.max(...this.state.restaurants.map(res => res.id))+1
+    const newRestaurant = {
+      id: newId,
+      name: this.state.newName,
+      rating: 0
+    }
+    this.setState((state) => ({restaurants: [...state.restaurants, newRestaurant]}))
   }
 
   render() {
@@ -23,6 +40,8 @@ class App extends Component {
         {this.state.restaurants.map(x => (
           <Restaurant id={x.id} name={x.name} rating={x.rating} />
         ))}
+        <input type="text" placeholder="Restaurant Name" value={this.state.newName} onChange={this.handleNameChage}/>
+        <button onClick={this.addRestaurant}>Add</button>
       </div>
     )
   }
